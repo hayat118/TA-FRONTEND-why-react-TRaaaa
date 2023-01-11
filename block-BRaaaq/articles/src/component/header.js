9f6b91cf1cfe28e25520c0c66f94fb423539fc57
ravi11o <ravi.suraj110@gmail.com>
2023-01-11 15:09:59 +0530

    add protypes in articles

---
+++ block-BRaaaq/articles/src/component/header.js
@@ -0,0 +1,27 @@
+function Header(){
+  return (
+    <> 
+    <header className="header">
+       <strong>Logo</strong>
+       <nav>
+         <ul>
+           <li>
+          
+             <a href="home">HOME</a>
+           </li>
+           <li>
+            
+               <a href="about">ABOUT</a>
+           </li>
+           <li>
+      
+               <a href="contact">CONTACT</a>
+           </li>
+         </ul>
+       </nav>
+    </header>
+    </>
+  )
+}
+
+export default Header;
