f32f01bc965a9a825f2d62f880f36c7b20b383a6
ravi11o <ravi.suraj110@gmail.com>
2023-01-13 17:33:14 +0530

    color-code

---
+++ block-BRaaay/code/color-code/src/component/app.js
@@ -0,0 +1,54 @@
+
+
+
+import Gray from './gray';
+import Red from './red';
+import Pink from "./pink";
+import Grape from "./grape";
+import Violet from "./violet";
+import Indigo from "./indigo";
+import Blue from "./blue";
+import Cyan from "./cyan";
+import Teal from "./teal";
+import Green from "./green";
+
+
+
+function App(){
+  return (
+    <>
+      <div className="color box">
+        <Gray />
+      </div>
+      <div className="color box">
+        <Red />
+      </div>
+      <div className="color box">
+        <Pink />
+      </div>
+      <div className="color box">
+        <Grape />
+      </div>
+      <div className="color box">
+        <Violet />
+      </div>
+      <div className="color box">
+        <Indigo />
+      </div>
+      <div className="color box">
+        <Blue />
+      </div>
+      <div className="color box">
+        <Cyan />
+      </div>
+      <div className="color box">
+        <Teal />
+      </div>
+      <div className="color box">
+        <Green />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ block-BRaaay/code/color-code/src/component/pink.js
@@ -0,0 +1,35 @@
+import data from "../data/colors.json";
+
+function Pink() {
+  return (
+    <>
+      {data.pink.map((color, i) => {
+        return (
+          <div
+            className="color"
+            style={{
+              backgroundColor: `${color}`,
+              height: "50px",
+              width: "100px",
+            }}
+          >
+            <div className="number">
+              <p>{more(i)}</p>
+              <p>{color}</p>
+            </div>
+          </div>
+        );
+      })}
+    </>
+  );
+}
+function more(i) {
+  var a = 100;
+  if (i === 0) {
+    return 50;
+  } else {
+    return a * i;
+  }
+}
+
+export default Pink;
